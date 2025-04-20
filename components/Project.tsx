a343650e91dece6d0f3b072143b66c4dda24f2b0
Muhammad Yudhistira <myudhistira07@gmail.com>
2025-04-20 21:35:03 +0700

    Adding New Project

--- components/Project.tsx
+++ components/Project.tsx
@@ -99,14 +99,45 @@ const Project = () => {
                   Github
                 </a>
               </Button>
-              {/* <Button size={'sm'}>
+            </div>
+          </div>
+        </motion.div>
+        <motion.div
+          className="bg-white/5 backdrop-blur-none rounded-xl w-full pb-4 flex flex-col start items-start"
+          initial={{ x: -200, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1 }}
+          viewport={{ once: true }}>
+          <div className="relative w-full aspect-[16/9] mb-4 overflow-hidden rounded-t-xl">
+            <Image
+              src={'/project/lppmweb2.png'}
+              alt="project"
+              fill
+              className="object-cover transition-transform duration-500 ease-in-out transform hover:scale-110"
+            />
+          </div>
+          <div className="flex flex-col gap-2 px-4">
+            <h4 className="text-xl font-bold">
+              Official Website Of LPPM Andalas University
+            </h4>
+            <p className="text-sm font-light">
+              The official website of the Institute for Research and Community
+              Service (LPPM) at Andalas University, providing information about
+              research and community service activities.
+            </p>
+            <div className="flex flex-wrap gap-1">
+              <Badge variant={'secondary'}>Wordpress</Badge>
+              <Badge variant={'secondary'}>Elementor</Badge>
+              <Badge variant={'secondary'}>CSS</Badge>
+              <Badge variant={'secondary'}>PHP</Badge>
+            </div>
+            <div className="flex gap-2 mt-2">
+              <Button size={'sm'}>
                 <Globe />
-                <a
-                  target="_blank"
-                  href="https://manajemen-aset.yudhistira.site">
+                <a target="_blank" href="https://lppm.unand.ac.id/">
                   Website
                 </a>
-              </Button> */}
+              </Button>
             </div>
           </div>
         </motion.div>
